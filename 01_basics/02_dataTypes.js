b022be0e14743a7082505d9795f4985a6b5a564b
Danish Abdullah <62188687+DanishSSE@users.noreply.github.com>
2024-07-10 05:24:41 +0000

    datatype practice

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Alert message") // avoid alert because we are using nodejs, not browser
+
+// code readability should be high
+// console.log(4 + 4) console.log("Danish")
+
+console.log(3 
+    + 3) 
+
+console.log("Danish")
+
+let name = "Danish"
+let age = 36
+let isLoggedIn = false
+let state
+
+// FIRST CATEGORY - Primitive Data Types
+// number => 2 to power 52 // equals to available memory address size maybe
+// bigint // for bigger number e.g trading, stock market or very big website
+// string => "" or '' // prefer to use double quotes
+// boolean => true/false
+// null => its a data type and as well standalone value but its type is object if you use typeof
+// undefined => means need to assign value to the variable // its a type
+// symbol => unique
+
+// SECOND CATEGORY - Data Types
+// arrays
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
